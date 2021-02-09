97c1a87f7f708943c796b670ac3d5089644edd86
Sophie Stammers <sophie.stammers@gmail.com>
2021-02-09 21:53:37 +0000

    alg: added number of images to recommend parametre

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ const alike = (score1, score2) => {
 
 // === Main function ===
 
-const similarImages = (chosenImage, matchChoice) => {
+const similarImages = (chosenImage, matchChoice, numberOfImages) => {
   // Create a list of keys to images from the data set
   const imageKeys = Object.keys(data); // [ '1.jpg',  '2.jpg',  '3.jpg', etc ...
 
@@ -44,7 +44,7 @@ const similarImages = (chosenImage, matchChoice) => {
   // Create an array from the matchRatings dictionary
   let imagesOrdered = Object.keys(matchRatings).map(key => [key, matchRatings[key]]);
   // Sort imagesOrdered based on highest to lowest match value
-  return imagesOrdered.sort((first, second) => second[1] - first[1]).slice(0, 6);
+  return imagesOrdered.sort((first, second) => second[1] - first[1]).slice(0, numberOfImages + 1);  
 };
 
 // The similarImages() function has 2 (compulsory) parameters: 
@@ -52,12 +52,12 @@ const similarImages = (chosenImage, matchChoice) => {
 // (ii) match function choice: high (which weights matches with high scores), or alike, which weights matches with similar scores.
 // e.g.
 
-console.log(similarImages(data["6.jpg"], high)); // recommends as similar, in desc, order: 5, 10, 14, 4, 18
-console.log(similarImages(data["6.jpg"], alike)); // recommends 10, 5, 14, 15, 11
-console.log(similarImages(data["1.jpg"], high)); // recommends 16, 18, 2, 11, 7
-console.log(similarImages(data["1.jpg"], alike)); // recommends 16, 15, 14, 11, 2
-console.log(similarImages(data["11.jpg"], high)); // recommends 16, 13, 18, 15, 2
-console.log(similarImages(data["11.jpg"], alike)); // recommends 15, 16, 14, 18, 13
+console.log(similarImages(data["6.jpg"], high, 5)); // recommends as similar, in desc, order: 5, 10, 14, 4, 18
+console.log(similarImages(data["6.jpg"], alike, 5)); // recommends 10, 5, 14, 15, 11
+console.log(similarImages(data["1.jpg"], high, 6)); // recommends 16, 18, 2, 11, 7, 15
+console.log(similarImages(data["1.jpg"], alike, 6)); // recommends 16, 15, 14, 11, 2, 18 
+console.log(similarImages(data["11.jpg"], high, 5)); // recommends 16, 13, 18, 15, 2
+console.log(similarImages(data["11.jpg"], alike, 5)); // recommends 15, 16, 14, 18, 13
 
 // The similarImages() function returns the a list of lists: each nested list contains the image key and the total match rating. It's this match rating by which the returned list is ordered: from highest similarity, descending. One could do further filtering with this information (e.g. return only the first few images if the latter's match rating is much lower)
-// Note that element 0 will always be the image we passed in. For efficiency over larger data sets, we'd remove this, but I find it interesting to see the comparison. 
+// Note that element 0 will always be the image we passed in. For efficiency over larger data sets, we'd remove this, but I find it interesting to see the comparison.
